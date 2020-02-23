2faa04de270032b3a637d21beb410caeea213286
Lebedeu <53447664+Lebedeu@users.noreply.github.com>
2020-02-23 10:21:43 -0300

    Ending Project People.

--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import axios from 'axios';
 
 import PeopleList from "../components/PeopleList";
@@ -9,33 +9,48 @@ export default class PeoplePage extends React.Component {
     super(props);
 
     this.state = {
-      peoples: []
+      peoples: [],
+      loading: false,
     };
   }
 
   componentDidMount() {
-    axios
-      .get('https://randomuser.me/api/?nat=br&results=15')
-      .then(response => {
-        const { results } = response.data;
-        this.setState({
-          peoples: results
-        });
-      })
+    this.setState({loading: true});
+    setTimeout(() => {
+      axios
+        .get('https://randomuser.me/api/?nat=br&results=15')
+        .then(response => {
+          const { results } = response.data;
+          this.setState({
+            peoples: results,
+            loading: false,
+          });
+        })
+    }, 1200)
   }
 
   render() {
     return (
-      // this.props.navigation.navigate('PeopleDetail'),
-      <View>
-        <PeopleList
-          peoples={this.state.peoples}
-          onPressItem={pageParams => {
-            this.props.navigation.navigate('PeopleDetail', pageParams);
-          }}
-        />
+      <View style={styles.container}>
+        {
+          this.state.loading
+            ? <ActivityIndicator size="large" color="#6ca2f7" />
+            : <PeopleList
+                peoples={this.state.peoples}
+                onPressItem={pageParams => {
+                  this.props.navigation.navigate('PeopleDetail', pageParams);
+                }}
+              />
+        }
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  }
+});
 
